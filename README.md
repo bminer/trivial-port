1f3fd4629f43ca385d8570c9ef57485fc4c359af
Blake Miner <miner.blake@gmail.com>
2014-02-10 17:47:46 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
 trivial-port
 ============
 
-Node.js library for opening serial ports for reading and writing
+Node.js library to open serial ports for reading and writing
+
+Operating System Support
+------------------------
+- **Linux** - Works
+- **Mac OSX** - May work with a few minor tweaks
+- **Windows** - It should work using the [`MODE`]
+(http://www.computerhope.com/modehlp.htm) command instead of `stty`.
+Checkout [this StackOverflow post](http://stackoverflow.com/questions/5728691/nodejs-reading-tty-serial)
+
+Why?
+----
+Mostly because node-serialport and other C/C++ Node add-ons are a bit buggy IMHO.
+
+Usage
+-----
+```javascript
+var SerialPort = require("trivial-port");
+var port = new SerialPort();
+port.initialize();
+port.on("data", function(chunk) {
+	console.log("Incoming:", chunk);
+})
+port.write("Hello, World!");
+```
+
+Trivial-port implements a [Duplex stream](http://nodejs.org/api/stream.html#stream_class_stream_duplex)
+for reading from and writing to a serial port.
+
+This implementation uses [`stty`](http://www.freebsd.org/cgi/man.cgi?query=stty&sektion=1)
+to initialize the stream; then, it simply reads from or writes to the serial
+port using the [`fs` Node.js library](http://nodejs.org/api/fs.html).
+
+
+### `var port = new SerialPort(options);`
+
+The options object allows you to pass named options to the serial port during
+initialization. The valid attributes for the options object are the following:
+
+- `serialPort` - the serial port device name (required)
+- `baudRate` - Baud Rate (defaults to 9600)
+- `dataBits` - Data Bits - Must be one of: 8, 7, 6, or 5. (defaults to 8)
+- `stopBits` - Stop Bits - Must be one of: 1 or 2. (defaults to 1)
+- `parity` - Parity - Must be one of: 'none', 'odd', 'even',
+	'mark', 'space' (defaults to 'none')
+- `sttyPath` - the path to the `stty` command (defaults to /bin/stty)
+- `initTimeout` - maximum initialization duration (defaults to 10 seconds);
+	set to `null` to allow an infinite amount of time
+
+### `port.initialize([cb])`
+
+Sets the appropriate serial port options and opens the serial port.  If `cb`
+is specified, it is called when the port has been opened. `cb` is of the form
+`cb(err)`.
+
+### `port.close([cb])`
+
+Closes the serial port.  If `cb` is specified, it is called when the port
+has been closed.  `cb` is of the form `cb(err)`.
+
+### Lots more methods
+
+The `SerialPort` class inherits all of the methods and events of [Readable]
+(http://nodejs.org/api/stream.html#stream_class_stream_readable) and [Writable]
+(http://nodejs.org/api/stream.html#stream_class_stream_writable) interfaces.
---
+++ serialport.js
@@ -0,0 +1,210 @@
+var child_process = require("child_process")
+	, stream = require("stream")
+	, fs = require("fs")
+	, os = require("os")
+	, util = require("util");
+
+/* Implements a Duplex stream for reading from and writing to a serial port.
+
+This implementation uses `stty` to initialize the stream; then, it simply reads
+from or writes to the serial port using the "fs" Node.js library.
+
+The options object allows you to pass named options to the serial port during
+initialization. The valid attributes for the options object are the following:
+
+- serialPort - the serial port device name (required)
+- baudRate - Baud Rate (defaults to 9600)
+- dataBits - Data Bits - Must be one of: 8, 7, 6, or 5. (defaults to 8)
+- stopBits - Stop Bits - Must be one of: 1 or 2. (defaults to 1)
+- parity - Parity - Must be one of: 'none', 'odd', 'even',
+	'mark', 'space' (defaults to 'none')
+- sttyPath - the path to the `stty` command (defaults to /bin/stty)
+- initTimeout - maximum initialization duration (defaults to 10 seconds);
+	set to `null` to allow an infinite amount of time
+*/
+function SerialPort(opts) {
+	stream.Duplex.call(this, {
+		//"allowHalfOpen": true
+	});
+	this.serialPort = opts.serialPort;
+	this.baudRate = opts.baudRate || 9600;
+	this.dataBits = opts.dataBits || 8;
+	this.stopBits = opts.stopBits || 1;
+	this.parity = opts.parity || 'none';
+	this.sttyPath = opts.sttyPath || "/bin/stty";
+	if(opts.initTimeout === undefined) {
+		opts.initTimeout = 10000;
+	}
+	this.initTimeout = opts.initTimeout;
+	this._fd = null;
+}
+util.inherits(SerialPort, stream.Duplex);
+module.exports = SerialPort;
+
+SerialPort.prototype.initialize = function(cb) {
+	var self = this;
+	//If there is no callback, then just emit errors
+	if(typeof cb !== "function") {
+		cb = function(err) {
+			if(err) {
+				self.emit("error", err);
+			}
+		};
+	}
+	//Check to see if initialization is complete
+	if(self._fd) {
+		return cb(new Error("SerialPort: Please close the port before reinitializing.") );
+	}
+	//Device name
+	if(self.serialPort == null) {
+		return cb(new Error("SerialPort: Path to serial port device was not specified.") );
+	}
+	var args = [];
+	if(os.platform() === "linux") {
+		args.push("-F");
+	}
+	else if(os.platform() === "darwin") {
+		args.push("-f");
+	}
+	else {
+		throw new Error("Operating system not supported.");
+	}
+	//Set a few sane options
+	args.push(self.serialPort, "raw", "-onlcr", "-iexten", "-echo",
+		"-echoe", "-echok", "-echoctl", "-echoke");
+	//Baud rate
+	args.push("speed", self.baudRate);
+	//Data bits
+	if(self.dataBits < 5 || self.dataBits > 8) {
+		return cb(new Error("SerialPort: Data byte must be between 5 and 8 bits.") );
+	}
+	args.push("cs" + self.dataBits);
+	//Stop bits
+	if(self.stopBits === 2) {
+		args.push("cstopb");
+	}
+	else if(self.stopBits === 1) {
+		args.push("-cstopb");
+	}
+	else {
+		return cb(new Error("SerialPort: Number of stop bits must be 1 or 2.") );
+	}
+	//Parity
+	switch(self.parity) {
+		case "none":
+			args.push("-parenb");
+			break;
+		case "odd":
+			args.push("parenb", "-parext", "parodd");
+			break;
+		case "even":
+			args.push("parenb", "-parext", "-parodd");
+			break;
+		case "mark":
+			args.push("parenb", "parext", "parodd");
+			break;
+		case "space":
+			args.push("parenb", "parext", "-parodd");
+			break;
+		default:
+			return cb(new Error("SerialPort: Invalid parity: " + self.parity) );
+	}
+	//Spawn stty process
+	var child = child_process.spawn(self.sttyPath, args),
+		initTimer;
+	//Set a timer just in case the process hangs
+	if(self.initTimeout != null) {
+		initTimer = setTimeout(function() {
+			child.process.removeListeners("exit", exitListener);
+			child.kill("SIGKILL");
+			cb(new Error("SerialPort: stty did not complete in a timely manner") );
+		}, self.initTimeout);
+	}
+	child.on("exit", exitListener);
+	function exitListener(code, signal) {
+		clearTimeout(initTimer);
+		if(code === 0) {
+			openSerialPortDevice();
+		}
+		else {
+			cb(new Error("SerialPort: stty returned exit code " + code) );
+		}
+	}
+	//Once the process exits successfully, we call this function
+	function openSerialPortDevice() {
+		fs.open(self.serialPort, "r+", function(err, fd) {
+			if(err) {
+				return cb(err);
+			}
+			//Save file descriptor and streams
+			self._fd = fd;
+			self._readStream = fs.createReadStream(self.serialPort, {"fd": fd});
+			self._writeStream = fs.createWriteStream(self.serialPort, {"fd": fd});
+			//Setup error handlers
+			self._readStream.on("error", function(err) {
+				self.emit("error", err);
+			});
+			self._writeStream.on("error", function(err) {
+				self.emit("error", err);
+			});
+			//Setup read event handlers
+			self._readStream.on("end", function() {
+				self.push(null);
+			});
+			self._readStream.on("close", function() {
+				self.emit("close");
+			});
+			//Emit open event
+			self.emit("open");
+		});
+	}
+};
+
+SerialPort.prototype._read = function(size) {
+	whenOpen(this, function() {
+		var self = this;
+		//Read a chunk from _readStream
+		var chunk = self._readStream.read();
+		if(chunk === null) {
+			//We can also read a chunk later once the stream is readable
+			self._readStream.once("readable", function() {
+				self.push(this.read() );
+			});
+		}
+		else {
+			self.push(chunk);
+		}
+	});
+};
+
+SerialPort.prototype._write = function(chunk, encoding, cb) {
+	whenOpen(this, function() {
+		//cb is called once data is flushed
+		this._writeStream.write(chunk, encoding, cb);
+	});
+};
+
+SerialPort.prototype.close = function(cb) {
+	var self = this;
+	//If there is no callback, then just emit errors
+	if(typeof cb !== "function") {
+		cb = function(err) {
+			if(err) {
+				self.emit("error", err);
+			}
+		};
+	}
+	//Close the device's file descriptor
+	var fd = self._fd;
+	self._fd = self._readStream = self._writeStream = null;
+	fs.close(fd, cb);
+};
+
+function whenOpen(self, cb) {
+	if(self._fd == null) {
+		self.on("open", cb);
+	}
+	else {
+		cb.call(self);
+	}
+}
