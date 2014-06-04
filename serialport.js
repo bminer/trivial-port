1685cdfcaaf06ced84952cef856eb76460d0670a
Mark Buster <mbuster@bitwisecontrols.com>
2014-06-04 11:18:16 -0600

    Requested PR Changes
    Switch to tabs for indent
    Rename  `customArgs` to `sttyArgs`
    Use `concat` instead of `push` for `stty` args

--- serialport.js
+++ serialport.js
@@ -22,7 +22,7 @@ initialization. The valid attributes for the options object are the following:
 - sttyPath - the path to the `stty` command (defaults to /bin/stty)
 - initTimeout - maximum initialization duration (defaults to 10 seconds);
 	set to `null` to allow an infinite amount of time
-- customArgs - an array of additional arguments to pass to the `stty` command
+- sttyArgs - an array of additional arguments to pass to the `stty` command
 */
 function SerialPort(opts) {
 	stream.Duplex.call(this, {
@@ -38,7 +38,7 @@ function SerialPort(opts) {
 		opts.initTimeout = 10000;
 	}
 	this.initTimeout = opts.initTimeout;
-  this.customArgs = opts.customArgs || [];
+	this.sttyArgs = opts.sttyArgs || [];
 	this._readStream = null;
 	this._writeStream = null;
 }
@@ -113,12 +113,10 @@ SerialPort.prototype.initialize = function(cb) {
 		default:
 			return cb(new Error("SerialPort: Invalid parity: " + self.parity) );
 	}
-  //customArgs
-  if(self.customArgs instanceof Array){
-    self.customArgs.forEach(function(arg){
-      args.push(arg);
-    });
-  }
+	//sttyArgs
+	if(self.sttyArgs instanceof Array){
+		args = args.concat(self.sttyArgs);
+	}
 	//Get the file descriptors for reading/writing the SerialPort
 	fs.open(self.serialPort, "r+", function(err, readFd) {
 		if(err) {
